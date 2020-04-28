776b7dbdc431d5ae568cab6f5120e8601c56d101
Rafael Marques <marquesrf@protonmail.com>
2020-04-28 17:11:29 -0300

    Removed test data and configurations

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = 'i-(s5X)]jxVpR&qGfLqT.2Tvnapg'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///myblog.db'
-    MAIL_SERVER = 'smtp.gmail.com'
+    SECRET_KEY = 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///your-db.db'
+    MAIL_SERVER = 'your-email-server' # gmail, outlook
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
